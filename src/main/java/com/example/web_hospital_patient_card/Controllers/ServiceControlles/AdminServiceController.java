d8eda25dac75fd9669b994e34b47db98b5d74cb9
Andrew Mykytyn <andrik.mykytyn@gmail.com>
2020-10-28 15:20:26 +0200

    Edit user, server side saving

--- src/main/java/com/example/web_hospital_patient_card/Controllers/ServiceControlles/AdminServiceController.java
+++ src/main/java/com/example/web_hospital_patient_card/Controllers/ServiceControlles/AdminServiceController.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.annotation.WebInitParam;
 import java.text.ParseException;
 import java.util.Collections;
 import java.util.List;
@@ -49,8 +50,8 @@ public class AdminServiceController {
                              @RequestParam(value = "email") String email,
                              @RequestParam(value = "sickness_name") String sicknessName,
                              @RequestParam(value = "pill_name") String pill_name,
-                             @RequestParam(value = "doctor_id") long doctor_id,
-                             @RequestParam(value = "recovery_period") int recovery_period,
+                             @RequestParam(value = "doctor_id",defaultValue = "0") long doctor_id,
+                             @RequestParam(value = "recovery_period",defaultValue = "0") int recovery_period,
                              @RequestParam(value = "last_attendance_date") String lastAttendanceDate) throws ParseException {
         System.out.println(name + "\n");
 
@@ -124,68 +125,64 @@ public class AdminServiceController {
 
     @PostMapping("/edit_user_data_submit")
     @ResponseBody
-    public String editUser(@RequestParam(value = "name") String name,
-                             @RequestParam(value = "surname") String surname,
-                             @RequestParam(value = "birth_date") String birthDate,
-                             @RequestParam(value = "email") String email,
-                             @RequestParam(value = "sickness_name") String sicknessName,
-                             @RequestParam(value = "pill_name") String pill_name,
-                             @RequestParam(value = "doctor_id") long doctor_id,
-                             @RequestParam(value = "recovery_period") int recovery_period,
-                             @RequestParam(value = "last_attendance_date") String lastAttendanceDate) throws ParseException {
+    public String editUser(@RequestParam(value = "name",defaultValue = "null") String name,
+                             @RequestParam(value = "surname",defaultValue = "null") String surname,
+                             @RequestParam(value = "birth_date",defaultValue = "null") String birthDate,
+                             @RequestParam(value = "email",defaultValue = "null") String email,
+                             @RequestParam(value = "sickness_name",defaultValue = "null") String sicknessName,
+                             @RequestParam(value = "pill_name",defaultValue = "null") String pill_name,
+                             @RequestParam(value = "doctor_id",defaultValue = "0") long doctor_id,
+                             @RequestParam(value = "recovery_period",defaultValue = "0") int recovery_period,
+                             @RequestParam(value = "last_attendance_date",defaultValue = "null") String lastAttendanceDate) throws ParseException {
 
 
         User user = userRepoServiceClass.getUserByEmail(email);
-        if (name == null) {
-            return "setNamePlease";
-        }
-        user.setName(name);
-        if (surname == null) {
-            return "setSurNamePlease";
-        }
-        user.setSurname(surname);
-
-        if (email == null) {
-            return "setEmailPlease";
+        if (!name.equals("null")) {
+            user.setName(name);
         }
 
-        user.setEmail(email);
-
-
-        if (recovery_period == 0) {
-            return "setRecoveryPeriodPlease";
-        }
-        user.setRecoveryPeriod(recovery_period);
-        if (birthDate == null) {
-            return "setDateOfBirthPlease";
-        }
-        user.setBirthDate(userServiceClass.getDateFromString(birthDate));
-        if (lastAttendanceDate == null) {
-            return "setLastAttendanceDate";
-        }
-        user.setLastAttendanceDate(userServiceClass.getDateFromString(lastAttendanceDate));
-
-        if (pill_name == null) {
-            return "setPillIdPlease";
+        if (!surname.equals("null")) {
+            user.setSurname(surname);
         }
 
-        Pill s = new Pill();
-        s.setName(pill_name);
-        user.setPillList(List.of(s));
-
-        if (sicknessName == null) {
-            return "setSicknessIdPlease";
+        if (!email.equals("null")) {
+            user.setEmail(email);
         }
 
-        Sickness sickness = new Sickness();
-        sickness.setName(sicknessName);
-        user.setSicknessList(List.of(sickness));
-
-        if (doctor_id == 0) {
-            return "setDoctorIDPlease";
+        if (recovery_period != 0) {
+            user.setRecoveryPeriod(recovery_period);
         }
 
-        user.setDoctor(doctorRepoServiceClass.getDoctorById(doctor_id));
+        if (!birthDate.equals("null")) {
+            user.setBirthDate(userServiceClass.getDateFromString(birthDate));
+        }
+
+        if (!lastAttendanceDate.equals("null")) {
+            user.setLastAttendanceDate(userServiceClass.getDateFromString(lastAttendanceDate));
+        }
+
+
+        if (!pill_name.equals("null")) {
+            Pill s = new Pill();
+            s.setName(pill_name);
+            user.getPillList().add(s);
+        }
+
+
+
+
+
+        if (!sicknessName.equals("null")) {
+            Sickness sickness = new Sickness();
+            sickness.setName(sicknessName);
+            user.getSicknessList().add(sickness);
+        }
+
+
+
+        if (doctor_id != 0) {
+            user.setDoctor(doctorRepoServiceClass.getDoctorById(doctor_id));
+        }
 
 
         userRepoServiceClass.updateUser(user);
